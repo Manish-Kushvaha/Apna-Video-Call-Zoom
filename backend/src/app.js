0d6925209bf16fac45218df840f630d9e8356235
Manish <kushvahamanish555@gmail.com>
2025-08-29 21:07:15 +0530

    Ready Deployement

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import { Server } from "socket.io";
+import express from "express";
+import { createServer } from "node:http";
+
+
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+
+import cors from "cors";
+import userRoutes from "./routes/users.routes.js"
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({ limit: "40kb", extended: true }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+app.use("/api/v1/users", userRoutes);
+// app.use("/api/v2/users", newUserRoutes);
+
+const start = async () => {
+    app.set("mongo_user");
+    const connectionDb = await mongoose.connect("mongodb+srv://kushvahamanish555:sitaramcodetechzoom04@cluster0.vzisgwc.mongodb.net/");
+    console.log(`MONGO Connected DB Host : ${connectionDb.connection.host}`);
+    server.listen(app.get("port"), () => {
+        console.log("Listen on port 8000")
+    });
+}
+
+start();
